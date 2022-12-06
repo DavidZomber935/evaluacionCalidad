c933ea22cfad49eb1e9468847f099725173f104d
DavidZomber935 <davidbra12345@gmail.com>
2022-12-06 10:15:04 -0500

    init commit

---
+++ src/main/java/ec/edu/itsqmet/evaluacion/EvaluacionCalidadApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionCalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionCalidadApplication.class, args);
+	}
+
+}
